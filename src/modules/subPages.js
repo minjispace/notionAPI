8a2b42d77eabc546ee9b42588d21129c5f0077e0
minji <321963741@naver.com>
2023-09-03 17:57:38 +0900

    feat: getSubPages funcgion logic added

---
+++ src/modules/subPages.js
@@ -0,0 +1,13 @@
+// 서브 페이지 리스트 조회 함수
+export function getSubPages(pageId, db, callback) {
+  const query = "SELECT * FROM Pages WHERE parent_page_id = ?";
+
+  db.query(query, [pageId], (error, results) => {
+    if (error) {
+      console.error("MySQL query error:", error);
+      callback([]);
+    } else {
+      callback(results);
+    }
+  });
+}
