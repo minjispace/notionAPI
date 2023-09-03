8e8cdc76a41a062b07a3a6d9a049b01edf8f44e0
minji <321963741@naver.com>
2023-09-03 17:58:55 +0900

    feat: routes logic added

---
+++ src/routes/api.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { getPageInfo, getBreadcrumbs, getSubPages } from "../modules/index.js";
+import { db } from "../db/index.js";
+
+const router = express.Router();
+
+router.get("/pages/:pageId", (req, res) => {
+  // get pageId
+  const pageId = req.params.pageId;
+
+  getPageInfo(pageId, db, (page) => {
+    if (!page) {
+      return res.status(404).json({ error: "Page not found" });
+    }
+
+    getSubPages(pageId, db, (subPages) => {
+      getBreadcrumbs(pageId, db, (breadcrumbs) => {
+        // 브로드 크럼스 배열을  띄어쓰기 없애고, 문자열로 변환
+        const breadcrumbsString = breadcrumbs
+          .map((crumb) => crumb.trim())
+          .join(" / ");
+
+        // return page info
+        res.status(200).json({
+          pageId: page.page_id,
+          title: page.title,
+          subPages: subPages.map((subPage) => ({
+            pageId: subPage.page_id,
+            title: subPage.title,
+          })),
+          breadcrumbs: breadcrumbsString,
+        });
+      });
+    });
+  });
+});
+
+export default router;
